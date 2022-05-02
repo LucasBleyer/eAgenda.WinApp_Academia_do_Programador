88255f6d42579a7b4079f29b251a13b1a9a2aaea
Lucas Bleyer <lucasbleyer19@gmail.com>
2022-05-02 16:06:30 -0300

    interface menu principal

---
+++ eAgenda.WinApp/MenuAgenda.Designer.cs
@@ -0,0 +1,102 @@
+﻿namespace eAgenda.WinApp
+{
+    partial class MenuAgenda
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(MenuAgenda));
+            this.painel_principal = new System.Windows.Forms.Panel();
+            this.button1 = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
+            this.button3 = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // painel_principal
+            // 
+            this.painel_principal.Location = new System.Drawing.Point(177, 12);
+            this.painel_principal.Name = "painel_principal";
+            this.painel_principal.Size = new System.Drawing.Size(611, 426);
+            this.painel_principal.TabIndex = 0;
+            // 
+            // button1
+            // 
+            this.button1.Font = new System.Drawing.Font("Unispace", 11.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point);
+            this.button1.Location = new System.Drawing.Point(22, 70);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(133, 69);
+            this.button1.TabIndex = 1;
+            this.button1.Text = "Tarefas";
+            this.button1.UseVisualStyleBackColor = true;
+            // 
+            // button2
+            // 
+            this.button2.Font = new System.Drawing.Font("Unispace", 11.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point);
+            this.button2.Location = new System.Drawing.Point(22, 185);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(133, 69);
+            this.button2.TabIndex = 2;
+            this.button2.Text = "Contatos";
+            this.button2.UseVisualStyleBackColor = true;
+            // 
+            // button3
+            // 
+            this.button3.Font = new System.Drawing.Font("Unispace", 11.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point);
+            this.button3.Location = new System.Drawing.Point(22, 304);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(133, 69);
+            this.button3.TabIndex = 3;
+            this.button3.Text = "Compromissos";
+            this.button3.UseVisualStyleBackColor = true;
+            // 
+            // MenuAgenda
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 15F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.button3);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.painel_principal);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle;
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.MaximizeBox = false;
+            this.MinimizeBox = false;
+            this.Name = "MenuAgenda";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "eAgenda";
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Panel painel_principal;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Button button3;
+    }
+}
