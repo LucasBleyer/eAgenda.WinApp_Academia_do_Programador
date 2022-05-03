f772b7d51fff1edc2c13b51d119f270193e9c74f
Lucas Bleyer <lucasbleyer19@gmail.com>
2022-05-03 11:36:33 -0300

    rename

--- eAgenda.WinApp/Properties/Resources.Designer.cs
+++ eAgenda.WinApp/Properties/Resources.Designer.cs
@@ -8,7 +8,7 @@
 // </auto-generated>
 //------------------------------------------------------------------------------
 
-namespace eAgenda.WinApp.Properties {
+namespace eAgenda.WinForms.Properties {
     using System;
     
     
@@ -39,7 +39,7 @@ namespace eAgenda.WinApp.Properties {
         internal static global::System.Resources.ResourceManager ResourceManager {
             get {
                 if (object.ReferenceEquals(resourceMan, null)) {
-                    global::System.Resources.ResourceManager temp = new global::System.Resources.ResourceManager("eAgenda.WinApp.Properties.Resources", typeof(Resources).Assembly);
+                    global::System.Resources.ResourceManager temp = new global::System.Resources.ResourceManager("eAgenda.WinForms.Properties.Resources", typeof(Resources).Assembly);
                     resourceMan = temp;
                 }
                 return resourceMan;
