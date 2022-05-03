8afd5b65cd77c6d38abaa9236f0627b98bdf23eb
Lucas Bleyer <lucasbleyer19@gmail.com>
2022-05-02 22:17:43 -0300

    consigo chamar a gestao de tarefas

--- eAgenda.WinApp/MenuAgenda.cs
+++ eAgenda.WinApp/MenuAgenda.cs
@@ -1,19 +1,26 @@
-﻿using GestaoTarefas.Infra;
-using GestaoTarefas.Dominio;
-using System;
-using System.Collections.Generic;
-using System.ComponentModel;
-using System.Data;
-using System.Drawing;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+﻿using System;
 using System.Windows.Forms;
+using GestaoTarefas.WinApp;
 
 namespace eAgenda.WinApp
 {
     public partial class MenuAgenda : Form
     {
-        
+        public MenuAgenda()
+        {        
+            InitializeComponent();
+        }
+
+        private void bt_menuTarefas_Click(object sender, EventArgs e)
+        {
+            ListagemTarefas tela = new ListagemTarefas();
+
+            DialogResult resultado = tela.ShowDialog();
+
+            if (resultado == DialogResult.OK)
+            {
+                tela.DialogResult = resultado;
+            }
+        }
     }
 }
